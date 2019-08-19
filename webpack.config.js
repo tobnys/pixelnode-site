724ccfa5b92b7c9f760963c639abc69d03bc2b0f
Tobias Nyström <tobnystrom@gmail.com>
2019-08-19 23:07:04 +0200

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/site.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),
+    publicPath: '/dist/',
+    host: '127.0.0.1',
+    port: 8080,
+    open: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+};
